98eafec358054b93f6ce82862560c78bdca18b7e
Camilo Canclini <camilocanclini@gmail.com>
2023-01-29 16:21:33 -0300

    Añadiendo métodos de PROCESS MODULE

--- CoreModules/process.js
+++ CoreModules/process.js
@@ -1,12 +1,35 @@
-'use strict'
-const process = require('process');
+console.log('\n P R O C E S S \n');
+
+const { dir } = require('console');
+const process = require('process'); //No es necesario, ya que es un objeto global
+
 //console.log(process);
 
 //Devuelve el directorio en el que se encuentra situada la CLI
 // Es similar a __dirname, pero la diferencia es que el anterior es relativo al script o archivo desde donde se llama y .cwd() depende de la consola
-console.log(`Working Directorie: ${process.cwd()}`);
+console.log(`\nWorking Directory:\n ${process.cwd()}\n`);
 
+//Devuelve un Arreglo con los parametros que enviamos al momento de ejecutar nuestra aplicación con:
+// node app.js arg1 arg2 ... argn
+console.log('\nargv:');
+dir(process.argv);
 
-console.log(`argv: ${process.argv}`);
+// Devuelve las Environment Variables del sistema donde nos encontramos
+console.log('\nenv:');
+dir(process.env);
 
-//console.log(process.env);
+//Algunas Environment Variables Importantes
+console.log('\nenv.LANG:');
+dir(process.env.LANG);
+console.log('\nenv.TEMP:');
+dir(process.env.TEMP);
+console.log('\nenv.Path:');
+dir(process.env.Path);
+console.log('\nenv.USERNAME:');
+dir(process.env.USERNAME);
+
+// Devuelve
+console.log('\n stdin:');
+console.log(process.stdout.write('holaaaaa\n'));
+
+console.log('\n +------------+ \n');
